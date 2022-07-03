ef243ffa78a1d89c8a861f3e4dfc8989ec6c64a6
xiraynedev <xiraynedev@gmail.com>
2022-07-03 13:20:42 -0400

    add next news project with images

--- src/components/ProjectComponent/ProjectComponent.tsx
+++ src/components/ProjectComponent/ProjectComponent.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useLayoutEffect} from 'react';
+import React, { FC, useLayoutEffect } from 'react';
 import gsap from 'gsap';
 
 interface ProjectProps {
@@ -12,35 +12,56 @@ interface ProjectProps {
   };
 }
 
-export const ProjectComponent: FC<ProjectProps> = ({packageProps}) => {
-  const {title, text, liveAnchorHref, sourceAnchorHref, imageFull, imageMedium} = packageProps;
+export const ProjectComponent: FC<ProjectProps> = ({ packageProps }) => {
+  const {
+    title,
+    text,
+    liveAnchorHref,
+    sourceAnchorHref,
+    imageFull,
+    imageMedium,
+  } = packageProps;
 
   useLayoutEffect(() => {
-    gsap.to('#project-section', {opacity: 1, duration: 2.5, delay: 1.75, ease: 'Bounce.easeOut'})
-  })
+    gsap.to('#project-section', {
+      opacity: 1,
+      duration: 2.5,
+      delay: 1.75,
+      ease: 'Bounce.easeOut',
+    });
+  });
 
   return (
     <section
-    id='project-section'
-      className='opacity-0 flex flex-col mx-auto mb-20 w-9/12 lg:flex-row lg:mt-12 lg:max-w-screen-lg'>
+      id='project-section'
+      className='opacity-0 flex flex-col mx-auto mb-20 w-9/12 lg:flex-row lg:mt-12 lg:max-w-screen-lg'
+    >
       <article className='my-8 md:mr-20'>
         <h3 className='font-bold text-5xl mb-5'>{title}</h3>
-        <p className='leading-normal text-3xl lg:my-8'>
-          {text}
-        </p>
+        <p className='leading-normal text-3xl lg:my-8'>{text}</p>
         <div className='flex items-center mt-8'>
-          <a href={liveAnchorHref} className='border w-48 p-4 shadow text-blue-900 font-bold text-3xl mr-8'>See Live</a>
-          {sourceAnchorHref &&
-						<a href={sourceAnchorHref} className='border w-52 p-4 shadow text-blue-900 font-bold text-3xl'>Source
-							Code</a>}
+          <a
+            href={liveAnchorHref}
+            className='border w-48 p-4 shadow text-blue-900 font-bold text-3xl mr-8'
+          >
+            See Live
+          </a>
+          {sourceAnchorHref && (
+            <a
+              href={sourceAnchorHref}
+              className='border w-60 p-4 shadow text-blue-900 font-bold text-3xl'
+            >
+              Source Code
+            </a>
+          )}
         </div>
       </article>
       <article className='my-8 shrink-0 w-full md:w-7/12 shadow border-0'>
         <div className='w-full'>
           <a href={liveAnchorHref}>
             <picture>
-              <source srcSet={imageFull} media='(min-width:800px)'/>
-              <img src={imageMedium} alt='React package search project.'/>
+              <source srcSet={imageFull} media='(min-width:800px)' />
+              <img src={imageMedium} alt='React package search project.' />
             </picture>
           </a>
         </div>
