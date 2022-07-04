4a185561fe0175dc5e8649f4733a56ea37f2e08c
xiraynedev <xiraynedev@gmail.com>
2022-07-04 12:30:29 -0400

    install react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {Container} from './components/Container/Container';
+import { BrowserRouter } from 'react-router-dom';
+import { Container } from './components/Container/Container';
 import './global.scss';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement);
-root.render(<Container />);
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLDivElement,
+);
+root.render(
+  <BrowserRouter>
+    <Container />
+  </BrowserRouter>,
+);
