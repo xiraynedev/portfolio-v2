63a663b9869d22cd11c0f36000ef551ac131e0ee
xiraynedev <xiraynedev@gmail.com>
2022-07-04 13:21:07 -0400

    implement react router

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,36 @@
-import React, {useLayoutEffect} from 'react';
+import React, { useLayoutEffect } from 'react';
+import { Link } from 'react-router-dom';
 import gsap from 'gsap';
 export const Header: React.FC = () => {
-
   useLayoutEffect(() => {
-    gsap.to('#top', {duration: 1.5, y: 0, ease: 'Bounce.easeOut'})
-  })
+    gsap.to('#top', { duration: 1.5, y: 0, ease: 'Bounce.easeOut' });
+  });
 
   return (
-    <header id='top' className='-translate-y-full text-white mx-auto bg-gradient-to-r from-yellow-900 to-yellow-500 py-20'>
+    <header
+      id='top'
+      className='-translate-y-full text-white mx-auto bg-gradient-to-r from-yellow-900 to-yellow-500 py-20'
+    >
       <section className='lg:max-w-screen-lg mx-auto'>
-        <h1 className='text-center text-6xl font-bold p-4 mb-5'>RAYMOND CORTES</h1>
+        <h1 className='text-center text-6xl font-bold p-4 mb-5'>
+          RAYMOND CORTES
+        </h1>
         <article>
-          <p className='w-9/12 text-3xl leading-normal my-8 mx-auto py-12'>I have worked with technology for many years. This portfolio contains work that began from scratch. As obstacles were encountered with code duplication and other problems, the portfolio has been refactored using several technologies that include HTML, CSS, JavaScript, React, Node, TypeScript, webpack, and more. The portfolio is updated as new projects are undertaken.
+          <p className='w-9/12 text-3xl leading-normal my-8 mx-auto py-12'>
+            I have worked with technology for many years. This portfolio
+            contains work that began from scratch. As obstacles were encountered
+            with code duplication and other problems, the portfolio has been
+            refactored using several technologies that include HTML, CSS,
+            JavaScript, React, Node, TypeScript, webpack, and more. The
+            portfolio is updated as new projects are undertaken.
           </p>
-          <a href='https://github.com/xiraynedev' className='block w-9/12 my-8 p-4 text-3xl mx-auto text-center border'>View
-            GitHub Profile</a>
+          <a
+            href='https://github.com/xiraynedev'
+            className='block w-9/12 my-8 p-4 text-3xl mx-auto text-center border'
+          >
+            View GitHub Profile
+          </a>
+          <Link to='/resume' className='block w-9/12 p-4 text-3xl mx-auto text-center border'>View Resume</Link>
         </article>
       </section>
     </header>
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Container } from './components/Container/Container';
+import { Resume } from './components/Resume/Resume';
 import './global.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLDivElement,
 );
 root.render(
-  <BrowserRouter>
-    <Container />
-  </BrowserRouter>,
+  <Router>
+    <Routes>
+      <Route path='/' element={<Container />} />
+      <Route path='/resume' element={<Resume />} />
+    </Routes>
+  </Router>,
 );
