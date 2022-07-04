29a0c2ee8c3c071e9972162f70d56cd2c9aa87c6
xiraynedev <xiraynedev@gmail.com>
2022-07-04 13:30:41 -0400

    add mui to project

--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -1,10 +1,18 @@
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
 import React, { FC } from 'react';
 
 export const Resume: FC = () => {
   return (
-    <div>
-      <h1>Raymond Cortes</h1>
-    </div>
+    <Stack direction='column' spacing={2} margin={4}>
+      <Stack>
+        <Typography variant='h2'>Raymond Cortes</Typography>
+        <Typography variant='h4'>xiraynedev@gmail.com</Typography>
+        <Typography variant='h4'>954-225-1870</Typography>
+        <Typography variant='h4'>Location</Typography>
+        <Typography variant='h4'>Coconut Creek, FL</Typography>
+      </Stack>
+    </Stack>
   );
 };
 
