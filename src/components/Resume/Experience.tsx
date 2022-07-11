0a5c86e55051221911da001f4ae017ff15870c15
xiraynedev <xiraynedev@gmail.com>
2022-07-11 14:08:41 -0400

    refactor experience component

--- src/components/Resume/Experience.tsx
+++ src/components/Resume/Experience.tsx
@@ -5,8 +5,9 @@ export const Experience: FC = () => {
   const experience = {
     title: 'Experience',
     description:
-      'Spent the last two years learning about many software technologies and building projects from scratch. Gained familiarity with build tools and Git and GitHub. Experienced with JavaScript and several frontend libraries and frameworks. Able to use Linux and the Windows Subsystem for Linux for development tasks.',
+      'Spent the last two years learning about many software technologies and building projects from scratch. Gained familiarity with build tools, Git, GitHub. Experienced with JavaScript and several frontend libraries and frameworks. Able to use Linux for development tasks.',
   };
 
   return <SectionComponent {...experience} />;
 };
+
