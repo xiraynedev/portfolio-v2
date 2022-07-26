c59caf77ffc8ccd43651daf7b8e49fddbed2ad6f
xiraynedev <xiraynedev@gmail.com>
2022-07-26 16:11:28 -0400

    fix typo in experience component

--- src/components/Resume/Experience.tsx
+++ src/components/Resume/Experience.tsx
@@ -5,7 +5,7 @@ export const Experience: FC = () => {
   const experience = {
     title: 'Experience',
     description:
-      'Spent the last two years learning about many software technologies and building projects from scratch. Gained familiarity with build tools, Git, GitHub. Experienced with JavaScript and several frontend libraries and frameworks. Able to use Linux for development tasks.',
+      'Spent the last two years learning about many software technologies and building projects from scratch. Gained familiarity with build tools, Git, and GitHub. Experienced with JavaScript and several frontend libraries and frameworks. Able to use Linux for development tasks.',
   };
 
   return <SectionComponent {...experience} />;
