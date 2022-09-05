95de46ee58f04e25c9aafac4e23d29c687859a36
xiraynedev <xiraynedev@gmail.com>
2022-09-05 09:04:44 -0400

    update education component

--- src/components/Resume/Education.tsx
+++ src/components/Resume/Education.tsx
@@ -1,26 +1,26 @@
-import Typography from '@mui/material/Typography';
-import React, { FC } from 'react';
-import { SectionComponent } from './SectionComponent';
+import Typography from "@mui/material/Typography";
+import React, { FC } from "react";
+import { SectionComponent } from "./SectionComponent";
 
 export const Education: FC = () => {
   const databaseEducation = {
-    title: 'Database Administration and Development',
+    title: "Database Administration and Development",
     description: `SetFocus - Parsipanny, New Jersey -
        Knowledge of SQL databases including designing, implementing, maintaining, and administering SQL Server databases.`,
   };
 
   const technicalEducation = {
-    title: 'Computer and Electronics Engineering Technology',
-    description:
-      `ITT Technical Institute - Davie, Florida - Knowledge of circuits, systems, and specialized techniques used in electronics and computer technology fields. Basic programming knowledge with several computer programming languages.`,
+    title: "Computer and Electronics Engineering Technology",
+    description: `Knowledge of circuits, systems, and specialized techniques used in electronics and computer technology fields. Acquired extensive programming knowledge with several computer programming languages.`,
   };
 
   return (
     <>
-      <Typography variant='h3' className='font-bold'>Education</Typography>
+      <Typography variant="h3" className="font-bold">
+        Education
+      </Typography>
       <SectionComponent {...databaseEducation} />
       <SectionComponent {...technicalEducation} />
     </>
   );
 };
-
