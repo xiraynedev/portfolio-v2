ac7aaaa25d4575c2a30c4e8f524c254739a53e70
xiraynedev <xiraynedev@gmail.com>
2022-09-06 16:08:56 -0400

    remove a project from portfolio

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,20 +1,19 @@
-import React, { useLayoutEffect } from 'react';
-import { Header } from '../Header/Header';
-import { ReactDesign } from '../ReactDesign/ReactDesign';
-import { Footer } from '../Footer/Footer';
-import { PackageSearch } from '../PackageSearch/PackageSearch';
-import { ReactSocial } from '../ReactSocial/ReactSocial';
-import gsap from 'gsap';
-import { NextNews } from '../NextNews/NextNews';
-import { ReactBasics } from '../ReactBasics/ReactBasics';
+import React, { useLayoutEffect } from "react";
+import { Header } from "../Header/Header";
+import { ReactDesign } from "../ReactDesign/ReactDesign";
+import { Footer } from "../Footer/Footer";
+import { PackageSearch } from "../PackageSearch/PackageSearch";
+import gsap from "gsap";
+import { NextNews } from "../NextNews/NextNews";
+import { ReactBasics } from "../ReactBasics/ReactBasics";
 
 export const Container: React.FC = () => {
   useLayoutEffect(() => {
-    gsap.to('#title', {
+    gsap.to("#title", {
       x: 0,
       duration: 1.5,
       delay: 0.75,
-      ease: 'Bounce.easeOut',
+      ease: "Bounce.easeOut",
     });
   });
 
@@ -22,14 +21,13 @@ export const Container: React.FC = () => {
     <>
       <Header />
       <h2
-        id='title'
-        className='-translate-x-full text-center mt-20 mb-8 lg:mb-20 text-6xl font-bold'
+        id="title"
+        className="-translate-x-full text-center mt-20 mb-8 lg:mb-20 text-6xl font-bold"
       >
         PROJECTS
       </h2>
       <NextNews />
       <ReactBasics />
-      <ReactSocial />
       <PackageSearch />
       <ReactDesign />
       <Footer />
