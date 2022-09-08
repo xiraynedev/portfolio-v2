2ddcb0d0a2ba8044978c10a0cab1c1020bf62a2c
xiraynedev <xiraynedev@gmail.com>
2022-09-08 10:47:36 -0400

    update Experience component

--- src/components/Resume/Experience.tsx
+++ src/components/Resume/Experience.tsx
@@ -4,8 +4,7 @@ import { SectionComponent } from "./SectionComponent";
 export const Experience: FC = () => {
   const experience = {
     title: "Experience",
-    description: `Currently working with Python, Django, and SQL to build full-stack Web applications. Have previously worked with
-Node.js, Next.js, and TypeScript to build microservices packaged with Docker and hosted on Microsoft Azure.`,
+    description: `Spent the last three years working with JavaScript and related libraries and frameworks. Have also worked with Python, Django, and SQL to build full-stack Web applications. Mostly work with Node.js, Next.js, and TypeScript to build microservices packaged with Docker and hosted on Microsoft Azure.`,
   };
 
   return <SectionComponent {...experience} />;
