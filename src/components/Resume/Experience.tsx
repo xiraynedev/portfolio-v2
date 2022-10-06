5d500323b5bfd5bbe83adbd859a9fdc9fd444f5d
xiraynedev <xiraynedev@gmail.com>
2022-10-05 23:03:09 -0400

    update resume experience component

--- src/components/Resume/Experience.tsx
+++ src/components/Resume/Experience.tsx
@@ -4,7 +4,11 @@ import { SectionComponent } from './SectionComponent';
 export const Experience: FC = () => {
   const experience = {
     title: 'Experience',
-    description: `Spent the last three years working with JavaScript and related libraries and frameworks. Mostly work with React, Next.js, and TypeScript to build a variety of projects that connect to APIs and databases in addition to building projects for cloud-native environments.`,
+    description: `Three years experience with JavaScript and related libraries and frameworks. Mostly work with React, Next.js, and
+TypeScript to build a variety of projects that connect to APIs and databases in addition to building projects for cloud-native environments.
+Completed Project: Next News - Next.js and TypeScript version of Hacker News complete with state management,
+login/logout functionality with Firebase Authentication, full-text search, forgot password reset option, sorting, submit
+posts, discuss comments, and pagination.`,
   };
 
   return <SectionComponent {...experience} />;
