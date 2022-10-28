2647e4c6d17fea52a6f4d401d6fa782440e0b18e
xiraynedev <xiraynedev@gmail.com>
2022-10-27 21:50:16 -0400

    update skill component

--- src/components/Resume/Skill.tsx
+++ src/components/Resume/Skill.tsx
@@ -4,7 +4,7 @@ import { SectionComponent } from './SectionComponent';
 export const Skill: FC = () => {
   const skills = {
     title: 'Skills',
-    description: `React, Next.js, TypeScript, JavaScript, Node.js, Redux, HTML, CSS, SCSS, Tailwind CSS, Java, Python, SQL, MongoDB, Django, Express.js, Git, GitHub, webpack, Maven, Linux, Docker, Google Cloud Platform, Amazon Web Services, Microsoft Azure`,
+    description: `React, Next.js, TypeScript, JavaScript, Node.js, Redux, HTML, CSS, SCSS, Tailwind CSS, Java, Python, C#, .NET, SQL, MongoDB, Django, Express.js, Git, GitHub, webpack, Maven, Linux, Docker, Google Cloud Platform, Amazon Web Services, Microsoft Azure`,
   };
 
   return <SectionComponent {...skills} />;
