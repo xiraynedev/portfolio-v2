249b36158e7f5835752acaeae706156932e1dc3e
xiraynedev <xiraynedev@gmail.com>
2022-11-30 16:12:52 -0500

    update Next News live URL

--- src/components/NextNews/NextNews.tsx
+++ src/components/NextNews/NextNews.tsx
@@ -8,7 +8,7 @@ export const NextNews: FC = () => {
   const nextNewsProps = {
     title: 'Next News',
     text: `Next News is a Next.js and TypeScript version of Hacker News. State management is handled using the Context API. Login functionality uses Firebase Authentication. Components are built with Material UI. Styling is handled via Tailwind CSS.`,
-    liveAnchorHref: 'https://next-news-project.vercel.app/new/1',
+    liveAnchorHref: 'https://next-news-v1.vercel.app/new/1',
     sourceAnchorHref: 'https://github.com/xiraynedev/next-news/tree/main',
     imageFull: nextNewsLarge,
     imageMedium: nextNewsMedium,
