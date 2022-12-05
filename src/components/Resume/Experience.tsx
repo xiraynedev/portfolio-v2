7a0e8e0059fceb5cf23115e33828c329348f7393
xiraynedev <xiraynedev@gmail.com>
2022-12-04 20:05:27 -0500

    update resume experience

--- src/components/Resume/Experience.tsx
+++ src/components/Resume/Experience.tsx
@@ -4,7 +4,7 @@ import { SectionComponent } from './SectionComponent';
 export const Experience: FC = () => {
   const experience = {
     title: 'Experience',
-    description: `Three years experience with JavaScript and related libraries and frameworks. Mostly work with React, Next.js, and TypeScript to build a variety of applications that connect to APIs and databases and work in cloud-native environments. Work in an agile environment while collaborating with team members. Implement CI/CD pipelines through GitHub Actions.`,
+    description: `Three years experience with JavaScript and related libraries and frameworks. Currently work with React, Next.js, and TypeScript to build a variety of applications that connect to APIs and databases and work in cloud-native environments. Work in an agile environment to quickly iterate over new ideas and push code to production. Maintain a portfolio of applications while keeping all packages up to date.`,
   };
 
   return <SectionComponent {...experience} />;
