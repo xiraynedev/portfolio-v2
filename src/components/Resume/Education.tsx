281467aebd77336f150b5e722905e69cd8a06482
xiraynedev <xiraynedev@gmail.com>
2022-12-08 12:45:50 -0500

    update education component

--- src/components/Resume/Education.tsx
+++ src/components/Resume/Education.tsx
@@ -4,13 +4,13 @@ import { SectionContainer } from './SectionContainer';
 
 export const Education: FC = () => {
   const databaseEducation = {
-    title: 'Database Administration and Development',
+    title: `Master's Certificate in Database Administration and Development`,
     description: `SetFocus - Parsipanny, New Jersey -
        Knowledge of SQL databases including designing, implementing, maintaining, and administering SQL Server databases.`,
   };
 
   const technicalEducation = {
-    title: 'Computer and Electronics Engineering Technology',
+    title: 'Associate of Science in Computer and Electronics Engineering Technology',
     description: `Knowledge of circuits, systems, and specialized techniques used in electronics and computer technology fields. Acquired extensive programming knowledge with several computer programming languages.`,
   };
 
