fa460e74c1c9d96c0a50d316ed862432adb02932
xiraynedev <xiraynedev@outlook.com>
2023-02-16 11:56:18 -0500

    remove resume directory and mui from application

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Container } from './components/Container/Container';
-import { Resume } from './components/Resume/Resume';
 import './global.scss';
 
 const root = ReactDOM.createRoot(
@@ -12,7 +11,6 @@ root.render(
   <Router>
     <Routes>
       <Route path='/' element={<Container />} />
-      <Route path='/resume' element={<Resume />} />
     </Routes>
   </Router>,
 );
